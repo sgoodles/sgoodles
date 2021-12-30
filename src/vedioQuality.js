8d4bd7d9d8a73bfea4b777101a6c19f27ce51540
Sneha <sneha.gupta@oodles.io>
2021-12-30 19:07:31 +0530

    Initial Commit

--- src/vedioQuality.js
+++ src/vedioQuality.js
@@ -1,86 +1,57 @@
 import React, { Component } from "react";
-import img from './img/India_flag_icon.png';
-import img1 from './img/download.png';
-import img2 from './img/images.png'
+class CheckboxComponent extends Component {
 
-class vedioQuality extends Component {
   constructor() {
-    super();
-    this.state = {
-      name: "React"
-    };
-    this.onValueChange = this.onValueChange.bind(this);
-    this.formSubmit = this.formSubmit.bind(this);
-  }
-
-  onValueChange(event) {
-    this.setState({
-      selectedOption: event.target.value
-    });
-  }
-
-  formSubmit(event) {
-    event.preventDefault();
-    console.log(this.state.selectedOption)
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.formSubmit} className="mx-10 my-10 px-5">
-        <div className="radio">
-          <label >
-            <input
-            
-              type="radio"
-              value="High Definition"
-              checked={this.state.selectedOption === "High Definition"}
-              onChange={this.onValueChange}
-            />
-            {/* <img src={img} className="px-5" width="80px"/> */}
-            High Definition
-          </label>
-        </div>
-        <div className="radio">
-          <label >
-            <input
-              type="radio"
-              value="Medium"
-              checked={this.state.selectedOption === "Medium"}
-              onChange={this.onValueChange}
-            />
-            {/* <img src={img1} className="px-5" width="80px"/> */}
-            Medium
-          </label>
-        </div>
-        <div className="radio">
-          <label >
-            <input
-              type="radio"
-              value="Low"
-              checked={this.state.selectedOption === "Low"}
-              onChange={this.onValueChange}
-            />
-            {/* <img src={img2} className="px-5" width="80px"/> */}
-            Low
-          </label>
-        </div>
-        <div className="checkbox my-10">
-          <label >
-            <input
-              type="checkbox"
-              value="highQuality"
-              onChange={this.onValueChange}
-            />
-            High Quality on Wifi
-          </label>
-        </div>
-        
-        <div>
-          Selected option is : {this.state.selectedOption}
-        </div>
-      </form>
-    );
+  super();
+  this.state = {
+    items: {}
   }
 }
 
-export default vedioQuality;
+handleCheckboxClick = (event) => {
+  const { name, value } = event.target;
+  const item = localStorage.getItem(name);
+  if (item) {
+    console.log(`Remove item name: ${name}, value: ${value}`);
+    localStorage.removeItem(name);
+    this.setState(state => delete state.items[name])
+  } else {
+    console.log(`Set item name: ${name}, value: ${value}`);
+    localStorage.setItem(name, value);
+    this.setState(state => state.items[name] = value);
+  }
+}
+
+getLocalStorageItems = () => {
+  const storageKeys = Object.keys(localStorage);
+  let obj = {};
+  for (let i = 0; i < storageKeys.length; i++) {
+    obj[ storageKeys[i] ] = localStorage.getItem(storageKeys[i]);
+  }
+  this.setState({ items: obj });
+}
+
+componentWillMount() {
+  this.getLocalStorageItems();
+}
+
+render() {
+  const { items } = this.state;
+  return (
+    <form className="mx-10">
+      <input type="radio" id="language1" name="language1" value="Hindi" checked={items["language1"] !== undefined} onChange={event => this.handleCheckboxClick(event)} />
+      <label for="language1"> High Definition</label><br />
+      <input type="radio" id="language2" name="language2" value="English" checked={items["language2"] !== undefined} onChange={event => this.handleCheckboxClick(event)} />
+      <label for="language2"> Medium</label><br />
+      <input type="radio" id="language3" name="language3" value="Tamil" checked={items["language3"] !== undefined} onChange={event => this.handleCheckboxClick(event)} />
+      <label for="language3"> Low</label><br /><br /><br />
+      <input type="checkbox" id="language4" name="language4" value="Telugu" checked={items["language4"] !== undefined} onChange={event => this.handleCheckboxClick(event)} />
+      <label for="language4"> High Quality On Wifi</label><br />
+      
+      
+    </form>
+  )
+}
+}
+
+export default CheckboxComponent;
